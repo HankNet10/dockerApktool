4052a25c0f9713d25838f35064b9ed6dec11d846
HankNet10 <hanknet10@gmail.com>
2024-07-20 14:50:49 +0800

    1

--- run/apk.sh
+++ run/apk.sh
@@ -13,24 +13,59 @@ apkname=${apkpath##*/}
 packname=$2
 outpath=$3
 codepath=${apkname%.*}
-dateTime=a$(date '+%s%N' |base64 | tr A-Z a-z | cut -c 18-23)   #$(date "+%y%m%d%H%M%S")
-newPackname=${packname%.*}.${dateTime}
+dateTime=$(date "+%y%m%d%H%M%S")   #$(date '+%s%N' |base64 | tr A-Z a-z | cut -c 18-23)
+newPackname=$packname$dateTime
 mkdir /app/apk
 mkdir /app/apk/nosign
 cp $apkpath /app/apk/
-keytool -genkey -keystore /app/apk/nosign/$dateTime.jks -keyalg RSA -validity 10000 -storepass 123456 -alias /app/apk/nosign/moguCA  -dname "CN=WE, OU=WE, O=WE, L=WE, ST=WE, C=ZH"
+keytool -genkey -keystore /app/apk/nosign/$dateTime.jks -keyalg RSA -validity 10000 -storepass 123456 -alias /app/apk/nosign/moguCA  -dname "CN=$dateTime, OU=$dateTime, O=$dateTime, L=$dateTime, ST=$dateTime, C=ZH"
 rm -r /app/apk/${codepath}
 java -jar /usr/local/apktool_2.9.3.jar d /app/apk/$apkname -o /app/apk/${codepath}
 
 cd /app/apk/$codepath
 chmod +x AndroidManifest.xml
 # 这个步骤修改包名。
-
-sed -i "1,3s/$packname/$newPackname/g" AndroidManifest.xml
-sed -i "s/$packname.provider/$newPackname.provider/g" AndroidManifest.xml
+match=$(grep -o "$packname" AndroidManifest.xml |wc -l)
+if [ $match -ge 1 ]
+then
+    sed -i "1,3s/$packname/$newPackname/g" AndroidManifest.xml
+else
+    echo 包名错误1
+    exit
+fi
+match=$(grep -o "$packname.provider" AndroidManifest.xml |wc -l)
+if [ $match -eq 2 ]
+then
+    sed -i "s/$packname.provider/$newPackname.provider/g" AndroidManifest.xml
+else
+    echo 包名错误2
+    exit
+fi
+match=$(grep -o "$packname.fileProvider" AndroidManifest.xml |wc -l)
+if [ $match -eq 2 ]
+then
 sed -i "s/$packname.fileProvider/$newPackname.fileProvider/g" AndroidManifest.xml
+else
+    echo 包名错误3
+    exit
+fi
+match=$(grep -o "$packname.autosize-init-provider" AndroidManifest.xml |wc -l)
+if [ $match -ge 1 ]
+then
 sed -i "s/$packname.autosize-init-provider/$newPackname.autosize-init-provider/g" AndroidManifest.xml
+else
+    echo 包名错误4
+    exit
+fi
+match=$(grep -o "$packname.androidx-startup" AndroidManifest.xml |wc -l)
+if [ $match -ge 1 ]
+then
 sed -i "s/$packname.androidx-startup/$newPackname.androidx-startup/g" AndroidManifest.xml
+else
+    echo 包名错误5
+    exit
+fi
+
 # 
 cd ..
 java -jar /usr/local/apktool_2.9.3.jar b /app/apk/$codepath -o /app/apk/nosign/mogu.nozipalign.apk
